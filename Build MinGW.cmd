fe1e08ae5f5ccafa0cd4728b1938f5ec2e2a944d
Mr-Ojii <okaschan@gmail.com>
2020-12-04 16:28:09 +0900

    Update Build MinGW.cmd

--- Build MinGW.cmd
+++ Build MinGW.cmd
@@ -1,1 +1,1 @@
-g++ -Wall -shared "-Wl,--dll,--enable-stdcall-fixup" -o shutdown_plugin.auf src/shutdown_plugin.cpp src/shutdown_plugin.def -lmsvcrt -lgcc_s -lstdc++ -lmingw32 -lkernel32 -lgcc -lmoldname -lmingwex -luser32 -ladvapi32 -lshell32 -m32
+g++ -Wall -march=i686 -shared "-Wl,--dll,--enable-stdcall-fixup" -o shutdown_plugin.auf src/shutdown_plugin.cpp src/shutdown_plugin.def -m32
